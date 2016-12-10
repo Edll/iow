3c2177bd49dc313d19aae341e717f2c4e7025c68
Edlly <edlly@edlly.de>
2016-12-10 16:13:04 +0100

    add unit test

---
+++ IowLibaryTest/LoadAssemblyDyn.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IowLibaryTest {
+    [TestClass]
+    public class LoadAssemblyDyn {
+
+        [TestMethod]
+        public void load() {
+        }
+    }
+}
