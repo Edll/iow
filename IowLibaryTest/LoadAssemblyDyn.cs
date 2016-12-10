7c57a2e1643ceb31ebbe79135be8d878a05c727b
Edlly <edlly@edlly.de>
2016-12-10 16:37:37 +0100

    unittest und assembly load

--- IowLibaryTest/LoadAssemblyDyn.cs
+++ IowLibaryTest/LoadAssemblyDyn.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Reflection;
+using IowLibary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IowLibaryTest {
     [TestClass]
-    public class LoadAssemblyDyn {
+    public class LoadAssemblyDynTest {
+
 
         [TestMethod]
-        public void load() {
+        public void LoadTest() {
+            LoadAssemblyDyn load = new LoadAssemblyDyn();
+            Assembly actual = load.Load("iowkit.dll");
+            Assert.IsNotNull(actual);
         }
     }
 }
---
+++ IowLibrary/LoadAssemblyDyn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace IowLibary {
+    /// <summary>
+    /// Loads the iowdll dynamical, this allows us to give feedback for the installation of the dll.
+    /// </summary>
+    public class LoadAssemblyDyn {
+
+        // das kann man sicher auch als programm einstellung machen...
+        private const string DefaultAssemblyFileName = "iowkit.dll";
+
+        private Assembly _assembly;
+
+        /// <summary>
+        /// Loads an assembly from the system  by the given  name
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Assembly Load(string name) {
+           
+                _assembly = Assembly.Load(name);
+        
+            return _assembly;
+        }
+    }
+}
